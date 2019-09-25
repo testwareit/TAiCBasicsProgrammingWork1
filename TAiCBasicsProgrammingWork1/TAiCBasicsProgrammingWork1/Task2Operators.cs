58f5bb770333d7b729a2e23c8c625f5f4fbb8fb3
Pawel Wlizlo <pw@testware.it>
2019-09-25 14:21:21 +0200

    Add 3rd task to task2

--- TAiCBasicsProgrammingWork1/TAiCBasicsProgrammingWork1/Task2Operators.cs
+++ TAiCBasicsProgrammingWork1/TAiCBasicsProgrammingWork1/Task2Operators.cs
@@ -11,9 +11,9 @@ namespace TAiCBasicsProgrammingWork1
         {
             /// TODO:
             /// 2.1 Uncomment below 
-            /// 2.2 Fill all missing data types below. Code must be compileable
+            /// 2.2 Fill all missing operators. Code must be compileable
             /// 2.3 To verify if results are OK, run code;
-            
+
             /*
             int a = 3  3;
             Console.WriteLine($"Result from 'a' should be 9. Actual {a}");
@@ -24,7 +24,7 @@ namespace TAiCBasicsProgrammingWork1
             int c = 9  2;
             Console.WriteLine($"Result from 'c' should be 1. Actual {c}");
             */
-///-----------------------------------------------------------------------------------------------------
+            ///-----------------------------------------------------------------------------------------------------
             /// TODO second task:
             /// 2.4 Uncomment below
             /// 2.5 Write missing code, code must be compileable and runnable
@@ -37,6 +37,18 @@ namespace TAiCBasicsProgrammingWork1
             Console.WriteLine("Person half age: "  personHalfAge);
             */
             ///
+
+            ///-----------------------------------------------------------------------------------------------------
+            /// TODO third task:
+            /// 2.7 Uncomment below
+            /// 2.8 Write missing code, code must be compileable and runnable
+            /// 2.9 To verify if results are OK, run code;
+
+            /*
+            Console.WriteLine($"6 is bigger than 5. Result should be true. Actual: {IsBiggerThan5}");
+            Console.WriteLine($"10 is smaller or equal to 11. Result should be true. Actual: {TenIsSmallerOrEqualToEleven}");
+            Console.WriteLine($"True is opposite to false. Result should be true. Actual: {TrueOppositeToFalse}");
+            */
         }
     }
 }
